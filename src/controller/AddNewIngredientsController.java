0033b3d45cec7b4319f7a5f65d3ad472f47179dc
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-03-19 23:06:18 +0700

    Add New Ingredients Feature Completed

--- src/controller/AddNewIngredientsController.java
+++ src/controller/AddNewIngredientsController.java
@@ -1,10 +1,18 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
+import tools.AlertTools;
+import tools.DatabaseTools;
 import tools.JavafxTools;
 
 public class AddNewIngredientsController {
@@ -14,6 +22,47 @@ public class AddNewIngredientsController {
 
     @FXML
     void addBtn(ActionEvent event) {
+
+        String ingredientsName = ingredientsNameTf.getText();
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        if (ingredientsName.isBlank()) {
+            AlertTools.setAlert("Error", null, "Please Input A Valid Name!", AlertType.ERROR);
+            return;
+        }
+
+        try {
+            conn = DatabaseTools.getConnection();
+            statement = conn.createStatement();
+            resultSet = statement
+                    .executeQuery("SELECT * FROM ingredients WHERE name = '" + ingredientsName + "'");
+
+            if (resultSet.next()) {
+                AlertTools.setAlert("Error", null, "Ingredients Name Is Exist!", AlertType.ERROR);
+
+                JavafxTools.setTextFieldEmpty(ingredientsNameTf);
+
+                return;
+            } else {
+
+                statement = conn.createStatement();
+                int result = statement
+                        .executeUpdate("INSERT INTO ingredients (name) VALUES ('" + ingredientsName + "')");
+
+                if (result > 0) {
+                    AlertTools.setAlert("Success", null, "Add Ingredients Successfully!", AlertType.INFORMATION);
+                } else {
+                    AlertTools.setAlert("Error", null, "Add Ingredients Failed!", AlertType.ERROR);
+                }
+
+                JavafxTools.setTextFieldEmpty(ingredientsNameTf);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
