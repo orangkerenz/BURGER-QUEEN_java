19ad7753262c80f1deb3f259c45af8bc1f57354b
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-03-21 00:50:11 +0700

    Add New Ingredients Feature Done

--- src/controller/AddNewIngredientsController.java
+++ src/controller/AddNewIngredientsController.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
