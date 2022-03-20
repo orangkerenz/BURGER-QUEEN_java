ace640ef8ab430558d6978392ca3c40d0334ef94
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-03-20 15:56:20 +0700

    list of menu view controller feature done

--- src/controller/ListOfMenuViewController.java
+++ src/controller/ListOfMenuViewController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -8,6 +13,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import model.Ingredients;
 import model.Menu;
+import tools.DatabaseTools;
+import tools.JavafxTools;
 
 public class ListOfMenuViewController {
 
@@ -23,6 +30,8 @@ public class ListOfMenuViewController {
     @FXML
     private TableView<Ingredients> table;
 
+    private Menu targetedMenu;
+
     @FXML
     public void initialize() {
         ingredientCol.setCellValueFactory(new PropertyValueFactory<>("ingredientsName"));
@@ -34,8 +43,40 @@ public class ListOfMenuViewController {
         JavafxTools.changeSceneMouseEvent(event, "../view/ListOfMenuPage.fxml");
     }
 
-    void setMenuName(Menu menu) {
+    void setMenu(Menu menu) {
+        this.targetedMenu = menu;
 
+        setMenuNameText();
+
+        setTableRecipes();
+    }
+
+    private void setTableRecipes() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DatabaseTools.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(
+                    "SELECT ingredients.name, recipes.quantity_in_grams FROM recipes, ingredients WHERE recipes.ingredient_id = ingredients.id AND recipes.menu_id = "
+                            + targetedMenu.getId());
+
+            while (rs.next()) {
+                table.getItems()
+                        .add(new Ingredients(rs.getString("name"), rs.getDouble("quantity_in_grams")));
+            }
+
+            DatabaseTools.closeQueryOperation(conn, stmt, rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setMenuNameText() {
+        MenuName.setText(targetedMenu.getName());
     }
 
 }
