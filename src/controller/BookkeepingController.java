e90ce16a58035161528f9cc496e3cbcbebb1b0d9
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-03-20 11:22:20 +0700

    Completed Bookkeeping Feature

--- src/controller/BookkeepingController.java
+++ src/controller/BookkeepingController.java
@@ -1,16 +1,18 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-
-import avafx.fxml.FXML;
-
+import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-
 import model.Transaction;
-
+import tools.AlertTools;
+import tools.DatabaseTools;
 import tools.JavafxTools;
 
 public class BookkeepingController {
@@ -36,6 +38,29 @@ public class BookkeepingController {
         dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
         typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
         amountCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conn = DatabaseTools.getConnection();
+            statement = conn.createStatement();
+            String sql = "SELECT * FROM transactions";
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                table.getItems().add(new Transaction(resultSet.getInt("id"), resultSet.getString("type"),
+                        resultSet.getDouble("price"), resultSet.getString("transaction_date")));
+            }
+
+            DatabaseTools.closeQueryOperation(conn, statement, resultSet);
+
+        } catch (Exception e) {
+            AlertTools.setAlert("Error", null, "Error", AlertType.ERROR);
+
+            e.printStackTrace();
+        }
 
     }
 
