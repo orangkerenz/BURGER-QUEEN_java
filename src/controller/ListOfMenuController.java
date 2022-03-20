2c171eb33ed5fabd5c4139c0fd1ffeda92725246
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-03-21 00:51:58 +0700

    List Of Menu Feature Completed

--- src/controller/ListOfMenuController.java
+++ src/controller/ListOfMenuController.java
@@ -1,13 +1,25 @@
 package controller;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 import model.Menu;
-
+import tools.AlertTools;
+import tools.DatabaseTools;
 import tools.JavafxTools;
 
 public class ListOfMenuController {
@@ -28,22 +40,100 @@ public class ListOfMenuController {
     public void initialize() {
         idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         menuCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        avaliableCol.setCellValueFactory(new PropertyValueFactory<>("avaliable"));
+        avaliableCol.setCellValueFactory(new PropertyValueFactory<>("availableString"));
+
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conn = DatabaseTools.getConnection();
+            statement = conn.createStatement();
+            String sql = "SELECT * FROM menus";
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                table.getItems().add(new Menu(resultSet.getInt("id"), resultSet.getString("name"),
+                        resultSet.getInt("available"), resultSet.getInt("price")));
+            }
+
+            DatabaseTools.closeQueryOperation(conn, statement, resultSet);
+
+        } catch (Exception e) {
+            AlertTools.setAlert("Error", null, "Error", AlertType.ERROR);
+        }
 
     }
 
     @FXML
     void addBtn(ActionEvent event) {
-
+        JavafxTools.changeSceneActionEvent(event, "../view/AddNewMenuPage.fxml");
     }
 
     @FXML
     void editBtn(ActionEvent event) {
+        Menu selectedMenu = table.getSelectionModel().getSelectedItem();
 
+        if (selectedMenu != null) {
+
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/EditMenuPage.fxml"));
+
+                Parent root = loader.load();
+
+                EditMenuController controller = loader.getController();
+
+                controller.setMenu(selectedMenu);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                Scene scene = new Scene(root);
+
+                stage.setScene(scene);
+
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            AlertTools.setAlert("Error", null, "No Selected Menu!", AlertType.ERROR);
+        }
     }
 
     @FXML
     void viewBtn(ActionEvent event) {
+        Menu selectedMenu = table.getSelectionModel().getSelectedItem();
+
+        if (selectedMenu != null) {
+
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/ListOfMenuViewPage.fxml"));
+
+                Parent root = loader.load();
+
+                ListOfMenuViewController controller = loader.getController();
+
+                controller.setMenu(selectedMenu);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                Scene scene = new Scene(root);
+
+                stage.setScene(scene);
+
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            AlertTools.setAlert("Error", null, "No Selected Menu!", AlertType.ERROR);
+        }
 
     }
 
